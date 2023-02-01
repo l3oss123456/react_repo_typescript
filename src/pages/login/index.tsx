a3e29f2d6938354b4ce64f306f90f0336595133e
boss <oncelemon02@gmail.com>
2023-02-01 16:03:28 +0700

    start react hook form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,42 +1,88 @@
 import React, { useState } from "react"
-import { setLocalStorage, removeLocalStorage } from "../../services/utils/Storage"
+import { setLocalStorage } from "../../services/utils/Storage"
 import { Navigate } from "react-router-dom"
+import { Box, IconButton, InputAdornment, TextField } from "@mui/material"
+import { useForm } from "react-hook-form"
 import Spacer from "../../components/Spacer"
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined"
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined"
 
 const Login = () => {
     const [isLogin, setIsLogin] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
+
+    const {
+        register,
+        handleSubmit,
+        // watch,
+        formState: { errors }
+    } = useForm()
+
+    console.log("errorsss", errors)
+
+    const handleClickShowPassword = () => {
+        setShowPassword((show) => !show)
+    }
+    const onSubmit = (data: Object) => {
+        console.log(data)
+        setLocalStorage(`loginInfo`, {
+            username: "test",
+            password: "1234",
+            firstName: "boss",
+            lastName: "kung"
+        })
+        setIsLogin(true)
+    }
 
     return (
         <Spacer>
             {isLogin && <Navigate to={`/`} replace />}
-            <p>username</p>
-            <input />
-            <br />
 
-            <p>password</p>
-            <input />
-            <br />
-
-            <button
-                onClick={() => {
-                    setLocalStorage(`loginInfo`, {
-                        username: "test",
-                        password: "1234",
-                        firstName: "boss",
-                        lastName: "kung"
-                    })
-                    setIsLogin(true)
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                style={{
+                    display: `flex`,
+                    flexDirection: `column`,
+                    alignItems: `center`,
+                    justifyContent: `center`,
+                    height: `100vh`
                 }}
             >
-                login
-            </button>
-            <button
-                onClick={() => {
-                    removeLocalStorage(`loginInfo`)
-                }}
-            >
-                Sign-out
-            </button>
+                <Box style={{ display: `flex`, flexDirection: `column`, justifyContent: `center` }}>
+                    <Box style={{ textAlign: `left`, width: `max-content` }}>
+                        <TextField label="username" variant="outlined" defaultValue="boss" {...register("username", { required: true })} />
+                        {errors.username && <p style={{ color: `red` }}>{`Invalid username!`}</p>}
+                    </Box>
+
+                    <Box style={{ marginTop: 20, width: 200 }}>
+                        <TextField
+                            type={showPassword ? `text` : `password`}
+                            label="password"
+                            variant="outlined"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={handleClickShowPassword} edge="end">
+                                            {showPassword ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                            defaultValue="kung"
+                            {...register("password", { required: true })}
+                        />
+                        {errors.password && <p style={{ color: `red` }}>{`Invalid password!`}</p>}
+                    </Box>
+                    <button
+                        disabled={Object.values(errors).length > 0}
+                        style={{
+                            marginTop: 20
+                        }}
+                    >
+                        Login
+                    </button>
+                </Box>
+            </form>
         </Spacer>
     )
 }
